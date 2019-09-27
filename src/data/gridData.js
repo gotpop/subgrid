2a0103c7c0689ed269112d37587e428c206082e4
gotpop <liam@gotpop.net>
2019-09-27 14:27:14 +0100

    Fully templated site

--- src/data/gridData.js
+++ src/data/gridData.js
@@ -1,7 +1,14 @@
 const goData = {
-    name: "Rich Bruen",
-    genre: "Noir",
-    nationality: "Irish"
+    title: "Subgrid",
+    items: [
+        {name: 'Item 1'},
+        {name: 'Item 2'},
+        {name: 'Item 3'},
+        {name: 'Item 4'},
+        {name: 'Item 5'},
+        {name: 'Item 6'},
+        {name: 'Item 7'}
+    ]
 };
 
 module.exports.goData = goData;
