253510a3fe93bc2b7ecc15e47af9b30d45bf0f65
gotpop <liam@gotpop.net>
2019-09-27 12:54:18 +0100

    Set up Nunjucks to uses templating and data

---
+++ nunjucks.config.js
@@ -0,0 +1,6 @@
+let gridData = require('./src/data/gridData');
+
+module.exports = {
+    root: './src/html',
+    data: gridData.goData
+}
---
+++ src/data/gridData.js
@@ -0,0 +1,7 @@
+const goData = {
+    name: "Rich Bruen",
+    genre: "Noir",
+    nationality: "Irish"
+};
+
+module.exports.goData = goData;
