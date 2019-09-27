852edf7a2b77462bc893c93f65d5116cb3c25a28
gotpop <liam@gotpop.net>
2019-09-27 14:46:13 +0100

    Templated header and footer

--- src/data/gridData.js
+++ src/data/gridData.js
@@ -8,7 +8,25 @@ const goData = {
         {name: 'Item 5'},
         {name: 'Item 6'},
         {name: 'Item 7'}
-    ]
+    ],
+    nav: [
+        {name: 'Item 1'},
+        {name: 'Item 2'},
+        {name: 'Item 3'},
+        {name: 'Item 4'},
+        {name: 'Item 5'},
+        {name: 'Item 6'},
+        {name: 'Item 7'}
+    ],
+    footer: [
+        {name: 'Item 1'},
+        {name: 'Item 2'},
+        {name: 'Item 3'},
+        {name: 'Item 4'},
+        {name: 'Item 5'},
+        {name: 'Item 6'},
+        {name: 'Item 7'}
+    ],
 };
 
 module.exports.goData = goData;
